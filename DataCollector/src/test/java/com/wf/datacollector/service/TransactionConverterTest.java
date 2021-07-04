272f085b308aba743897fee1fe8d8f1a0e1cc4d6
subho <souvikdutta6635h@gmail.com>
2021-07-04 20:03:59 +0530

    Updated

--- DataCollector/src/test/java/com/wf/datacollector/service/TransactionConverterTest.java
+++ DataCollector/src/test/java/com/wf/datacollector/service/TransactionConverterTest.java
@@ -1,4 +1,29 @@
 package com.wf.datacollector.service;
 
+
+import com.wf.datacollector.filter.inbound.InboundFilter;
+import com.wf.datacollector.filter.outbound.OutboundFilter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class TransactionConverterTest {
+    private OutboundFilter outboundFilterChain;
+    private InboundFilter inboundFilter;
+
+    @Autowired
+    public void setOutboundFilterChain(OutboundFilter outboundFilterChain) {
+        this.outboundFilterChain = outboundFilterChain;
+    }
+
+    @Autowired
+    public void setInboundFilter(InboundFilter inboundFilter) {
+        this.inboundFilter = inboundFilter;
+    }
+
+    @Test
+    @DisplayName("Should return true since the Inbound Transaction was Okay")
+    public void testProduceIsTrue() {
+
+    }
 }
