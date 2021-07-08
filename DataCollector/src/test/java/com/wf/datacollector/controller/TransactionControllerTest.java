20eb2c3b4be08f3b4cfb995e5b4876f8f552ce71
Vanshika <vanshika.jain2000@gmail.com>
2021-07-09 02:35:32 +0530

    updated

--- DataCollector/src/test/java/com/wf/datacollector/controller/TransactionControllerTest.java
+++ DataCollector/src/test/java/com/wf/datacollector/controller/TransactionControllerTest.java
@@ -8,15 +8,18 @@ import com.wf.datacollector.service.TransactionConverter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureMockMvc
 @WebMvcTest(TransactionController.class)
 public class TransactionControllerTest {
     @Autowired
@@ -31,6 +34,7 @@ public class TransactionControllerTest {
     @MockBean
     private CollectedTransactionProducer collectedTransactionProducer;
 
+    @Autowired
     protected MockMvc mockMvc;
 
     @BeforeEach
@@ -38,6 +42,17 @@ public class TransactionControllerTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
+//    @Test
+//    public void consumeTransactionTest() throws Exception {
+//        InboundTransaction inboundTransaction = new InboundTransaction();
+//        when(collectedTransactionProducer.produce(inboundTransaction)).thenReturn(false) ;
+//
+//        mockMvc.perform(
+//                MockMvcRequestBuilders.get("/transaction"))
+//                .andExpect(status().isOk())
+//                .andExpect() ;
+//        )
+//    }
     @Test
     public void shouldFilterOutDebitCardTransactions() throws Exception {
         InboundTransaction inboundTransaction = new InboundTransaction();
