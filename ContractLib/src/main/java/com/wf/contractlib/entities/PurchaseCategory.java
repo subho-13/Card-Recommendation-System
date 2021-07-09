d5d72c0f39f0fce32903ce49878e5c1d871cac91
Sabuj Jana <sabujlovescoding@gmail.com>
2021-07-09 19:01:38 +0530

    updt

--- ContractLib/src/main/java/com/wf/contractlib/entities/PurchaseCategory.java
+++ ContractLib/src/main/java/com/wf/contractlib/entities/PurchaseCategory.java
@@ -1,61 +1,60 @@
 package com.wf.contractlib.entities;
 
 public enum PurchaseCategory {
-    PERSONAL,
-    HEALTH,
-    MISC_POS,
-    TRAVEL,
-    KIDS_PETS,
-    FOOD,
-    HOME,
+    EDUCATION,
     ENTERTAINMENT,
-    SHOP_POS,
-    SHOP_NET,
-    MISC_NET,
-    GROCERY_POS,
+    FOOD,
     GAS_TRANS,
     GROCERY_NET,
-    EDUCATION,
+    GROCERY_POS,
+    HEALTH,
+    HOME,
     HOTEL,
-    ANY,
+    KIDS_PETS,
+    MISC_NET,
+    MISC_POS,
+    PERSONAL,
+    SHOP_NET,
+    SHOP_POS,
+    TRAVEL,
     UNKNOWN;
 
     public static PurchaseCategory convert(String category) {
         switch (category) {
-            case "Personal":
-                return PERSONAL;
-            case "Health":
-                return HEALTH;
-            case "Misc_pos":
-                return MISC_POS;
-            case "Travel":
-                return TRAVEL;
-            case "Kids_pets":
-                return KIDS_PETS;
-            case "Shop_pos":
-                return SHOP_POS;
-            case "Food":
-                return FOOD;
-            case "Home":
-                return HOME;
-            case "Entertainment":
-                return ENTERTAINMENT;
-            case "Shop_net":
-                return SHOP_NET;
-            case "Misc_net":
-                return MISC_NET;
-            case "Grocery_pos":
-                return GROCERY_POS;
-            case "Gas_trans":
-                return GAS_TRANS;
-            case "Grocery_net":
-                return GROCERY_NET;
-            case "Education":
-                return EDUCATION;
-            case "Hotel":
-                return HOTEL;
-            case "Any":
-                return ANY;
+        case "Personal":
+            return PERSONAL;
+        case "Health":
+            return HEALTH;
+        case "Misc_pos":
+            return MISC_POS;
+        case "Travel":
+            return TRAVEL;
+        case "Kids_pets":
+            return KIDS_PETS;
+        case "Shop_pos":
+            return SHOP_POS;
+        case "Food":
+            return FOOD;
+        case "Home":
+            return HOME;
+        case "Entertainment":
+            return ENTERTAINMENT;
+        case "Shop_net":
+            return SHOP_NET;
+        case "Misc_net":
+            return MISC_NET;
+        case "Grocery_pos":
+            return GROCERY_POS;
+        case "Gas_trans":
+            return GAS_TRANS;
+        case "Grocery_net":
+            return GROCERY_NET;
+        case "Education":
+            return EDUCATION;
+        case "Hotel":
+            return HOTEL;
+        case "Any":
+            return ANY;
         }
         return UNKNOWN;
     }
