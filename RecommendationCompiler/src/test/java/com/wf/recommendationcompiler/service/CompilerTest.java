49276c8153db08487ae07d78c81ae30149b6dce8
Vanshika <vanshika.jain2000@gmail.com>
2021-07-11 02:42:30 +0530

    updated

---
+++ RecommendationCompiler/src/test/java/com/wf/recommendationcompiler/service/CompilerTest.java
@@ -0,0 +1,104 @@
+package com.wf.recommendationcompiler.service;
+
+import com.wf.contractlib.entities.CardType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompilerTest {
+
+    @InjectMocks
+    private Compiler compiler;
+    private Object NullPointerException;
+
+    @BeforeEach
+    void setUp() {
+        compiler = new Compiler();
+    }
+
+    @Test
+    @DisplayName("final card confidence map is returned successfully ")
+    public void getCardConfidenceMapTest() {
+        Map<CardType, Float> model1_map = new HashMap<>();
+        Map<CardType, Float> model2_map = new HashMap<>();
+        Map<CardType, Float> model3_map = new HashMap<>();
+
+        model1_map.put(CardType.CASH_WISE, 0.1f);
+        model1_map.put(CardType.COLLEGE, 0.02f);
+        model1_map.put(CardType.CREDIT_BUILDER, 0.03f);
+        model1_map.put(CardType.ENTERTAINMENT, 0.07f);
+        model1_map.put(CardType.HOLIDAY, 0.08f);
+        model1_map.put(CardType.HOTEL, 0.12f);
+        model1_map.put(CardType.PLATINUM, 0.18f);
+        model1_map.put(CardType.SHOPPING, 0.1f);
+        model1_map.put(CardType.VISA_SIGNATURE, 0.09f);
+
+        model2_map.put(CardType.CASH_WISE, 0.08f);
+        model2_map.put(CardType.COLLEGE, 0.01f);
+        model2_map.put(CardType.CREDIT_BUILDER, 0.1f);
+        model2_map.put(CardType.ENTERTAINMENT, 0.23f);
+        model2_map.put(CardType.HOLIDAY, 0.12f);
+        model2_map.put(CardType.HOTEL, 0.09f);
+        model2_map.put(CardType.PLATINUM, 0.43f);
+        model2_map.put(CardType.SHOPPING, 0.1f);
+        model2_map.put(CardType.VISA_SIGNATURE, 0.6f);
+
+        model3_map.put(CardType.CASH_WISE, 0.22f);
+        model3_map.put(CardType.COLLEGE, 0.09f);
+        model3_map.put(CardType.CREDIT_BUILDER, 0.01f);
+        model3_map.put(CardType.ENTERTAINMENT, 0.08f);
+        model3_map.put(CardType.HOLIDAY, 0.076f);
+        model3_map.put(CardType.HOTEL, 0.43f);
+        model3_map.put(CardType.PLATINUM, 0.5f);
+        model3_map.put(CardType.SHOPPING, 0.082f);
+        model3_map.put(CardType.VISA_SIGNATURE, 0.21f);
+
+        Map<String, Map<CardType, Float>> modelCardConfidenceMap = new HashMap<>();
+        modelCardConfidenceMap.put("Rule Learning", model1_map);
+        modelCardConfidenceMap.put("Association Rule Learning", model2_map);
+        modelCardConfidenceMap.put("Som Based", model3_map);
+
+        Map<CardType, Float> finalCardConfidencemap = new HashMap<>();
+
+        finalCardConfidencemap.put(CardType.CASH_WISE, 0.065f);
+        finalCardConfidencemap.put(CardType.COLLEGE, 0.0155f);
+        finalCardConfidencemap.put(CardType.CREDIT_BUILDER, 0.0405f);
+        finalCardConfidencemap.put(CardType.ENTERTAINMENT, 0.099f);
+        finalCardConfidencemap.put(CardType.HOLIDAY, 0.0616f);
+        finalCardConfidencemap.put(CardType.HOTEL, 0.0925f);
+        finalCardConfidencemap.put(CardType.PLATINUM, 0.22749999f);
+        finalCardConfidencemap.put(CardType.SHOPPING, 0.0582f);
+        finalCardConfidencemap.put(CardType.VISA_SIGNATURE, 0.24450001f);
+
+        assertEquals(finalCardConfidencemap, compiler.getCardConfidenceMap(modelCardConfidenceMap));
+
+    }
+
+
+    @Test
+    @DisplayName("Model weights are updated successfully ")
+    public void updateMapTest() {
+        HashMap<String, Float> modelWeightMap = new HashMap<>();
+        modelWeightMap.put("Rule Learning", 0.05F);
+        modelWeightMap.put("Association Rule Learning", 0.15F);
+        modelWeightMap.put("Kpca Kmeans", 0.35F);
+        modelWeightMap.put("Kpca Similarity", 0.06F);
+        modelWeightMap.put("Pca Kmeans", 0.04F);
+        modelWeightMap.put("Som Based", 0.1F);
+        modelWeightMap.put("Neural Based", 0.1F);
+        modelWeightMap.put("Autoencoder Based", 0.1F);
+        modelWeightMap.put("New User", 0.05F);
+
+        compiler.updateMap(modelWeightMap);
+        assertEquals(modelWeightMap, compiler.getModelWeightMap() );
+
+
+    }
+}
---
+++ RecommendationCompiler/src/test/java/com/wf/recommendationcompiler/service/DatabaseHandlerTest.java
@@ -0,0 +1,119 @@
+package com.wf.recommendationcompiler.service;
+
+import com.wf.contractlib.contracts.GeneratedRecommendation;
+import com.wf.contractlib.entities.CardType;
+import com.wf.recommendationcompiler.entity.RecommendationDetails;
+import com.wf.recommendationcompiler.repository.RecommendationDetailsRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+class DatabaseHandlerTest {
+
+    @InjectMocks
+    DatabaseHandler databaseHandler ;
+    @Mock
+    private RecommendationDetailsRepository recommendationDetailsRepository;
+    @Mock
+    private DetailsGenerator detailsGenerator;
+    @Mock
+    AutoCloseable closeable ;
+
+    @BeforeEach
+    void setUp() {
+        databaseHandler=new DatabaseHandler() ;
+        closeable= MockitoAnnotations.openMocks(this) ;
+
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        closeable.close();
+    }
+
+    public Map<CardType,Float> getMap()
+    {
+        Map<CardType,Float> confidenceMap = new HashMap<>() ;
+        confidenceMap.put(com.wf.contractlib.entities.CardType.CASH_WISE, 0.1f) ;
+        confidenceMap.put(com.wf.contractlib.entities.CardType.COLLEGE, 0.02f) ;
+        confidenceMap.put(com.wf.contractlib.entities.CardType.CREDIT_BUILDER, 0.03f) ;
+        confidenceMap.put(com.wf.contractlib.entities.CardType.ENTERTAINMENT, 0.07f) ;
+        confidenceMap.put(com.wf.contractlib.entities.CardType.HOLIDAY, 0.08f) ;
+        confidenceMap.put(com.wf.contractlib.entities.CardType.HOTEL, 0.12f) ;
+        confidenceMap.put(com.wf.contractlib.entities.CardType.PLATINUM, 0.18f) ;
+        confidenceMap.put(com.wf.contractlib.entities.CardType.SHOPPING, 0.1f) ;
+        confidenceMap.put(com.wf.contractlib.entities.CardType.VISA_SIGNATURE,0.09f) ;
+
+        return confidenceMap ;
+    }
+
+    @Test
+    @DisplayName("when customer is present")
+    public void handlerTest()
+    {
+        Map<CardType,Float> confidenceMap = getMap();
+
+        RecommendationDetails recommendationDetails = new RecommendationDetails() ;
+        recommendationDetails.setRecommendationID(001);
+        recommendationDetails.setCustomerID(01);
+        recommendationDetails.setModelName("Model_1");
+        recommendationDetails.setCardConfidenceMap(confidenceMap);
+
+        GeneratedRecommendation generatedRecommendation = new GeneratedRecommendation() ;
+        generatedRecommendation.setCustomerID(01);
+        generatedRecommendation.setModelName("Model_1");
+        generatedRecommendation.setCardConfidenceMap(confidenceMap);
+
+        when(detailsGenerator.generate(any())).thenReturn(recommendationDetails) ;
+        when(recommendationDetailsRepository.findByCustomerIDAndModelName(anyInt(),anyString())).thenReturn(Optional.of(recommendationDetails)) ;
+
+        databaseHandler.handle(generatedRecommendation);
+        verify(recommendationDetailsRepository,times(1)).save(any());
+        assertEquals(true,databaseHandler.getTestVariable());
+    }
+
+    @Test
+    @DisplayName("when customer is not present")
+    public void detailsNullTest()
+    {
+        Map<CardType,Float> confidenceMap = getMap();
+
+        RecommendationDetails recommendationDetails = new RecommendationDetails() ;
+        recommendationDetails.setRecommendationID(001);
+        recommendationDetails.setCustomerID(01);
+        recommendationDetails.setModelName("Model_1");
+        recommendationDetails.setCardConfidenceMap(confidenceMap);
+
+        GeneratedRecommendation generatedRecommendation = new GeneratedRecommendation() ;
+        generatedRecommendation.setCustomerID(01);
+        generatedRecommendation.setModelName("Model_1");
+        generatedRecommendation.setCardConfidenceMap(confidenceMap);
+
+        when(detailsGenerator.generate(any())).thenReturn(recommendationDetails) ;
+        when(recommendationDetailsRepository.findByCustomerIDAndModelName(anyInt(),anyString())).thenReturn(Optional.empty()) ;
+
+        databaseHandler.handle(generatedRecommendation);
+        verify(recommendationDetailsRepository,times(1)).save(any());
+        assertEquals(false,databaseHandler.getTestVariable());
+    }
+
+    @Test
+    public void getAllRecommendationDetailsTest()
+    {
+        databaseHandler.getAllRecommendationDetails();
+        when(recommendationDetailsRepository.findAll()).thenReturn(null) ;
+        verify(recommendationDetailsRepository,times(1)).findAll() ;
+    }
+}
