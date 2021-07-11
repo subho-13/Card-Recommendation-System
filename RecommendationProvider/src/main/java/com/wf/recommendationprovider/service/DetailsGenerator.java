5f8f3fde4653846b8fe20ea31738bf7bb36932ac
Vanshika <vanshika.jain2000@gmail.com>
2021-07-11 17:22:29 +0530

    updated

--- RecommendationProvider/src/main/java/com/wf/recommendationprovider/service/DetailsGenerator.java
+++ RecommendationProvider/src/main/java/com/wf/recommendationprovider/service/DetailsGenerator.java
@@ -3,11 +3,12 @@ package com.wf.recommendationprovider.service;
 import com.wf.contractlib.contracts.CompiledRecommendation;
 import com.wf.contractlib.contracts.featurevector.FeatureVectorOne;
 import com.wf.recommendationprovider.entity.CustomerDetails;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DetailsGenerator {
-    public CustomerDetails generate(FeatureVectorOne featureVectorOne) {
+    public CustomerDetails generate(@NotNull FeatureVectorOne featureVectorOne) {
         CustomerDetails customerDetails = new CustomerDetails();
         customerDetails.setCustomerID(featureVectorOne.getCustomerID());
         customerDetails.setJob(featureVectorOne.getJob());
@@ -18,9 +19,9 @@ public class DetailsGenerator {
         return customerDetails;
     }
 
-    public CustomerDetails generate(CompiledRecommendation compiledRecommendation) {
+    public CustomerDetails generate(@NotNull CompiledRecommendation compiledRecommendation) {
         CustomerDetails customerDetails = new CustomerDetails();
-        customerDetails.setCustomerID(compiledRecommendation.getCustomerId());
+        customerDetails.setCustomerID(compiledRecommendation.getCustomerID());
         customerDetails.setCardConfidenceMap(compiledRecommendation.getCardConfidenceMap());
         return customerDetails;
     }
