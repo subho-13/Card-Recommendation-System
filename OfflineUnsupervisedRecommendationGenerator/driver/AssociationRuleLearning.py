5ea5b480e0f6b79e335208edea21a06482014250
subho <souvikdutta6635h@gmail.com>
2021-07-11 23:05:01 +0530

    Updated

--- OfflineUnsupervisedRecommendationGenerator/driver/AssociationRuleLearning.py
+++ OfflineUnsupervisedRecommendationGenerator/driver/AssociationRuleLearning.py
@@ -1,16 +1,1 @@
-from driver.main import main
-from entities.FeatureVectorOne import engine
-from repository.DatabaseHandler import check_if_user_is_new, write_to_db
-from models.AssociationRuleLearning import association_rule_learning
-from util.Adapter import generate_feature_vector_one
-
-
-def to_send(User_Id):
-    return not check_if_user_is_new(User_Id)
-
-
-def model_driver():
-    return association_rule_learning("feature_vector_one", engine)
-
-
-main(model_driver, "AssociationRuleLearning", to_send, "FeatureVectorOne", generate_feature_vector_one, write_to_db)
+from models.SelfOrganizingMaps import
--- OfflineUnsupervisedRecommendationGenerator/driver/SelfOrganizingMaps.py
+++ OfflineUnsupervisedRecommendationGenerator/driver/SelfOrganizingMaps.py
@@ -1,16 +1,5 @@
-from driver.main import main
-from entities.FeatureVectorOne import engine
-from repository.DatabaseHandler import check_if_user_is_new, write_to_db
 from models.SelfOrganizingMaps import self_organizing_maps
-from util.Adapter import generate_feature_vector_one
+from driver.main import main
 
-
-def to_send(User_Id):
-    return not check_if_user_is_new(User_Id)
-
-
-def model_driver():
-    return self_organizing_maps("feature_vector_one", engine)
-
-
-main(model_driver, "SelfOrganizingMaps", to_send, "FeatureVectorOne", generate_feature_vector_one, write_to_db)
+if __name__ == "__main__":
+    main(self_organizing_maps, "SelfOrganizingMaps", False)
