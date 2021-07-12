4808ea64c904f9343e7229118cbc9c08262fd9ba
subho <souvikdutta6635h@gmail.com>
2021-07-12 21:22:19 +0530

    Updated

--- RecommendationCompiler/src/main/java/com/wf/recommendationcompiler/service/GeneratedRecommendationConsumer.java
+++ RecommendationCompiler/src/main/java/com/wf/recommendationcompiler/service/GeneratedRecommendationConsumer.java
@@ -3,7 +3,9 @@ package com.wf.recommendationcompiler.service;
 import com.wf.contractlib.contracts.GeneratedRecommendation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GeneratedRecommendationConsumer {
     private DatabaseHandler databaseHandler;
 
